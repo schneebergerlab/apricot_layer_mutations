8b4eb628303938ed8234119dcade709fb0a11e18
mnshgl0110 <mnshgl0110@gmail.com>
2023-07-10 17:30:07 +0200

    Batch scripts for loading reads to IGV

---
+++ SH/igv_bat.all_iso.sh
@@ -0,0 +1,12 @@
+new
+genome /netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/data/assemblies/hifi_assemblies/cur.genome.v1.fasta
+
+load /netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/isoseq/get_cells/WT_1/WT_1.iso_seq.bam
+load /netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/isoseq/get_cells/WT_19/WT_19.iso_seq.bam
+load /netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/isoseq/get_cells/MUT_11_1/MUT_11_1.iso_seq.bam
+load /netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/isoseq/get_cells/MUT_15/MUT_15.iso_seq.bam
+
+colorBy READ_STRAND
+setTrackHeight 25
+setDataRange
+squish
---
+++ SH/igv_bat.all_rna.sh
@@ -0,0 +1,11 @@
+new
+genome /netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/data/assemblies/hifi_assemblies/cur.genome.v1.fasta
+load /netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scrna/bigdata/scrna_clusters/WT_1/WT_1.dedup.pos_sorted.bam
+load /netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scrna/bigdata/scrna_clusters/WT_19/WT_19.dedup.pos_sorted.bam
+load /netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scrna/bigdata/scrna_clusters/MUT_11_1/MUT_11_1.dedup.pos_sorted.bam
+load /netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scrna/bigdata/scrna_clusters/MUT_15/MUT_15.dedup.pos_sorted.bam
+
+colorBy READ_STRAND
+setTrackHeight 25
+setDataRange
+squish
