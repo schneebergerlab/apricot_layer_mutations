62bd1a7ec2b6a3cd87765723b145e3860b7c93b2
mnshgl0110 <mnshgl0110@gmail.com>
2023-12-07 13:56:14 +0100

    Cleanup

--- R/presentation_plots.R
+++ R/presentation_plots.R
@@ -50,27 +50,6 @@ ggsave(plt, filename = '/local/goel/Dropbox/PostDoc_LMU/presentations/department
        dpi=1200,
        bg='transparent')
 
-## Draw heatmap for poster @ PAG2023
-rcvalues <- read.delim('/netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scdna/bigdata/variant_calling/layer_samples/all_layer_somatic_variants.read_counts.txt')
-# rcvalues <- t(read.delim('/netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scdna/bigdata/variant_calling/layer_samples/all_layer_somatic_variants.read_counts.txt'))
-afvalues <- read.delim('/netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scdna/bigdata/variant_calling/layer_samples/all_layer_somatic_variants.allele_freq.txt')
-annorow <- read.delim('/netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scdna/bigdata/variant_calling/layer_samples/all_layer_somatic_variants.annocol.txt')
-annocol <- read.delim('/netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scdna/bigdata/variant_calling/layer_samples/all_layer_somatic_variants.annorow.txt')
-rownames(rcvalues) <- paste(rcvalues$X, rcvalues$X.1, rcvalues$X.2, sep='_')
-rownames(afvalues) <- paste(afvalues$X, afvalues$X.1, afvalues$X.2, sep='_')
-rownames(annorow) <- colnames(rcvalues[4:32])
-rownames(annocol) <- rownames(rcvalues)
-ann_colors = list(
-  var_type = c(Indel='#B9D61A', SNP='#361AD5'),
-  branch=c('mut_11_1'='#3182bd','mut_11_2'='#6baed6','mut_15'='#9ecae1','mut_4'='#c6dbef',
-           'wt_1'='#e6550d', 'wt_7'='#fd8d3c', 'wt_18'='#fdae6b', 'wt_19'='#fdd0a2'),
-  tissue=c('L1'='#006d2c', 'L2'='#31a354', 'L3'='#74c476', 'leaf'='#e6550d')
-)
-color = colorRampPalette(c("white", "seagreen"))(50)
-## These plots were updated on 09.01.2023 after sending the poster for PAG. CHANGE THE POSITION OF MARGIN BASED ON THE NUMBER OF SNPs/INDELs
-pheatmap(t(rcvalues[4:32]), cluster_rows = FALSE, cluster_cols=FALSE, annotation_row = annorow[3], annotation_col = annocol[4], color=color, annotation_colors=ann_colors, show_rownames = FALSE, show_colnames=FALSE, gaps_row=c(0,0,0,8, 15, 22), gaps_col=c(79), fontsize_row=40, filename='/netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scdna/bigdata/variant_calling/layer_samples/all_layer_somatic_variants.read_counts.png', width=8, height=4, border_color=NA, main='Read count at layer-specific somatic mutation positions')
-pheatmap(t(afvalues[4:32]), cluster_rows = FALSE, cluster_cols=FALSE, annotation_row = annorow[3], annotation_col = annocol[4], color=color, annotation_colors=ann_colors, show_rownames = FALSE, show_colnames=FALSE, gaps_row=c(0,0,0,8, 15, 22), gaps_col=c(79), fontsize_row=60, annotation_names_row = FALSE, annotation_names_col = FALSE, filename ='/netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scdna/bigdata/variant_calling/layer_samples/all_layer_somatic_variants.allele_freq.png', width=8, height=4, border_color=NA, main='Allele frequency at layer-specific somatic mutation positions')
-
 ## Draw heatmap for all_sm_in_all_samples
 # rcvalues <- read.delim('/netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scdna/bigdata/variant_calling/all_sm_in_all_samples.read_counts.txt')
 # rcvalues <- t(read.delim('/netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scdna/bigdata/variant_calling/layer_samples/all_layer_somatic_variants.read_counts.txt'))
@@ -81,8 +60,8 @@ annocol <- read.delim('/netscratch/dep_mercier/grp_schneeberger/projects/apricot
 rownames(afvalues) <- paste(afvalues$X, afvalues$X.1, afvalues$X.2, sep='_')
 rownames(annorow) <- colnames(afvalues[4:24])
 rownames(annocol) <- rownames(afvalues)
-#
-#
+
+
 # colour = {('L1', 'SNP'): '#3274a1',
 # ('L1', 'Indel'): '#74A6C6',
 # ('L2', 'SNP'): '#e1812c',
@@ -102,5 +81,6 @@ ann_colors = list(
 color = colorRampPalette(c("white", "#540d6e"))(50) # Using colour Theme 1
 ## These plots were updated on 09.01.2023 after sending the poster for PAG. CHANGE THE POSITION OF MARGIN BASED ON THE NUMBER OF SNPs/INDELs
 # pheatmap(t(rcvalues[4:32]), cluster_rows = FALSE, cluster_cols=FALSE, annotation_row = annorow[3], annotation_col = annocol[4], color=color, annotation_colors=ann_colors, show_rownames = FALSE, show_colnames=FALSE, gaps_row=c(0,0,0,8, 15, 22), gaps_col=c(79), fontsize_row=40, filename='/netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scdna/bigdata/variant_calling/layer_samples/all_layer_somatic_variants.read_counts.png', width=8, height=4, border_color=NA, main='Read count at layer-specific somatic mutation positions')
-pheatmap(t(afvalues[4:24]), cluster_rows = FALSE, cluster_cols=FALSE, annotation_row = annorow[3], annotation_col = annocol[5], color=color, annotation_colors=ann_colors, show_rownames = FALSE, show_colnames=FALSE, gaps_row=c(0,0,0,7, 14), gaps_col=c(145, 155), annotation_names_row = FALSE, annotation_names_col = FALSE, filename ='/netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scdna/bigdata/variant_calling/all_sm_in_all_samples.allele_freq.png', width=5, height=3, border_color='black', main='Allele frequency at somatic mutation positions')
+pheatmap(t(afvalues[4:24]), cluster_rows = FALSE, cluster_cols=FALSE, annotation_row = annorow[3], annotation_col = annocol[5], color=color, annotation_colors=ann_colors, show_rownames = FALSE, show_colnames=FALSE, gaps_row=c(0,0,0,7, 14), gaps_col=c(145, 155), annotation_names_row = FALSE, annotation_names_col = FALSE, filename ='/netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scdna/bigdata/variant_calling/all_sm_in_all_samples.allele_freq.png', width=5, height=3, border_color='black')
+pheatmap(t(afvalues[4:24]), cluster_rows = FALSE, cluster_cols=FALSE, annotation_row = annorow[3], annotation_col = annocol[5], color=color, annotation_colors=ann_colors, show_rownames = FALSE, show_colnames=FALSE, gaps_row=c(0,0,0,7, 14), gaps_col=c(145, 155), annotation_names_row = FALSE, annotation_names_col = FALSE, filename ='/netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scdna/bigdata/variant_calling/all_sm_in_all_samples.allele_freq.pdf', width=5, height=3, border_color='black')
 
