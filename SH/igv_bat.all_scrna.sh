119e5ac32a88d9ebcc9e11d11bdc6bff226b3215
mnshgl0110 <mnshgl0110@gmail.com>
2023-12-07 13:58:19 +0100

    renamed

--- SH/igv_bat.all_scrna.sh
+++ SH/igv_bat.all_scrna.sh
@@ -5,6 +5,12 @@ load /netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scrn
 load /netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scrna/bigdata/scrna_clusters/MUT_11_1/MUT_11_1.dedup.pos_sorted.bam
 load /netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scrna/bigdata/scrna_clusters/MUT_15/MUT_15.dedup.pos_sorted.bam
 
+load /netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scrna/bigdata/scrna_clusters/WT_1/WT_1.merged.bam
+load /netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scrna/bigdata/scrna_clusters/WT_19/WT_19.merged.bam
+load /netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scrna/bigdata/scrna_clusters/MUT_11_1/MUT_11_1.merged.bam
+load /netscratch/dep_mercier/grp_schneeberger/projects/apricot_leaf/results/scrna/bigdata/scrna_clusters/MUT_15/MUT_15.merged.bam
+
+
 colorBy READ_STRAND
 setTrackHeight 25
 setDataRange
